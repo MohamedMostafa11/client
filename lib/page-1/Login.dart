6bbdfee06d18196ff261f579962b837cfacd0230
Mohamed Mostafa <@MohamedMostafa11>
2023-05-10 15:41:21 +0300

    change Image and Solve last error

--- lib/page-1/Login.dart
+++ lib/page-1/Login.dart
@@ -3,7 +3,6 @@ import 'package:flutter/gestures.dart';
 import 'dart:ui';
 import 'register.dart';
 import 'Admin.dart';
-import 'package:client/Model/Model.dart';
 import 'package:http/http.dart' as http;
 
 class LoginPage extends StatefulWidget {
@@ -19,6 +18,7 @@ class _LoginPageState extends State<LoginPage> {
     var _phoneNumber;
     var _password;
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.teal,
@@ -30,131 +30,133 @@ class _LoginPageState extends State<LoginPage> {
             body: Form(
               key: formKey,
               child: Center(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                        margin: EdgeInsets.all(25.0),
-                        height: 200,
-                        width: 200,
-                        decoration: BoxDecoration(
-                          border: Border.all(width: 5, color: Colors.teal),
-                          borderRadius: BorderRadius.circular(200),
-                        ),
-                        child: Image.asset(
-                          'assets/icon2.png',
-                        )),
-                    Container(
-                      padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.teal),
-                            borderRadius: BorderRadius.circular(90.0),
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                          margin: EdgeInsets.all(25.0),
+                          height: 200,
+                          width: 200,
+                          decoration: BoxDecoration(
+                            border: Border.all(width: 5, color: Colors.teal),
+                            borderRadius: BorderRadius.circular(200),
                           ),
-                          labelText: 'Phone Number',
-                        ),
-                        controller: _phoneNumber,
-
-                        validator: (value){
-                          if(value!.isEmpty){
-                            return 'Please enter phone number';
-                          }
-                          if(!RegExp("^[0]+[1]+[0,1,2,5]+[0-9]").hasMatch(value)){
-                            return 'Please enter valid number';
-                          }
-                          return null;
-                        },
-                      ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-                      child: TextFormField(
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(90.0),
+                          child: Image.asset(
+                            'assets/icon2.png',
+                          )),
+                      Container(
+                        padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.teal),
+                              borderRadius: BorderRadius.circular(90.0),
+                            ),
+                            labelText: 'Phone Number',
                           ),
-                          labelText: 'Password',
+                          controller: _phoneNumber,
+
+                          validator: (value){
+                            if(value!.isEmpty){
+                              return 'Please enter phone number';
+                            }
+                            if(!RegExp("^[0]+[1]+[0,1,2,5]+[0-9]").hasMatch(value)){
+                              return 'Please enter valid number';
+                            }
+                            return null;
+                          },
                         ),
-                        controller: _password,
-                        validator: (value){
-                          if(value!.isEmpty){
-                            return 'Please enter your Password';
-                          }
-                          if(value.length < 8){
-                            return 'Enter 8-Digits Password';
-                          }
-
-                          return null;
-                        },
                       ),
-                    ),
-                    Column(
-                      children: [
-                        Container(
-                            height: 100,
-                            padding: const EdgeInsets.all(20),
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(90.0)),
-                            child: ElevatedButton(
-                                style: ElevatedButton.styleFrom(
-                                  minimumSize: const Size.fromHeight(50),
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(30.0)),
-                                ),
-                                child: const Text('Log In',
-                                    style: TextStyle(
-                                        color: Colors.white, fontSize: 22)),
+                      Container(
+                        padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+                        child: TextFormField(
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(90.0),
+                            ),
+                            labelText: 'Password',
+                          ),
+                          controller: _password,
+                          validator: (value){
+                            if(value!.isEmpty){
+                              return 'Please enter your Password';
+                            }
+                            if(value.length < 8){
+                              return 'Enter 8-Digits Password';
+                            }
 
-                                onPressed: () {
-                                  //final phoneNumber = _phoneNumber.text;
-                                  //final password = _password.text;
-                                  //login(phoneNumber, password);
+                            return null;
+                          },
+                        ),
+                      ),
+                      Column(
+                        children: [
+                          Container(
+                              height: 100,
+                              padding: const EdgeInsets.all(20),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(90.0)),
+                              child: ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    minimumSize: const Size.fromHeight(50),
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(30.0)),
+                                  ),
+                                  child: const Text('Log In',
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 22)),
 
-                                  //Navigator.push(context, MaterialPageRoute(builder: (context) => Admin()));
+                                  onPressed: () {
+                                    //final phoneNumber = _phoneNumber.text;
+                                    //final password = _password.text;
+                                    //login(phoneNumber, password);
 
-                                  if(formKey.currentState!.validate()){
-                                  Navigator.push(context,
-                                  MaterialPageRoute(builder: (context) => Admin()));
+                                    //Navigator.push(context, MaterialPageRoute(builder: (context) => Admin()));
+
+                                    if(formKey.currentState!.validate()){
+                                    Navigator.push(context,
+                                    MaterialPageRoute(builder: (context) => Admin()));
+                                    }
                                   }
-                                }
 
 
-                                )
-                        ),
-                        Container(
-                          child: Text(
-                            'Don’t have an account yet?',
-                            style: TextStyle(
-                              fontFamily: 'Open Sans',
-                              fontWeight: FontWeight.w500,
-                              color: Color.fromARGB(255, 0, 0, 0),
+                                  )
+                          ),
+                          Container(
+                            child: Text(
+                              'Don’t have an account yet?',
+                              style: TextStyle(
+                                fontFamily: 'Open Sans',
+                                fontWeight: FontWeight.w500,
+                                color: Color.fromARGB(255, 0, 0, 0),
+                              ),
                             ),
                           ),
-                        ),
-                        Container(
-                            height: 80,
-                            padding: const EdgeInsets.all(20),
-                            child: ElevatedButton(
-                                style: ElevatedButton.styleFrom(
-                                  minimumSize: const Size.fromHeight(50),
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(30.0)),
-                                ),
-                                child: const Text('Sign Up',
-                                    style: TextStyle(
-                                        color: Colors.white, fontSize: 22)),
-                                onPressed: () {
-                                  Navigator.push(context,
-                                      MaterialPageRoute(builder: (context) {
-                                    return RegisterPage();
-                                  }));
-                                })),
-                      ],
-                    ),
-                  ],
+                          Container(
+                              height: 90,
+                              padding: const EdgeInsets.all(20),
+                              child: ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    minimumSize: const Size.fromHeight(50),
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(30.0)),
+                                  ),
+                                  child: const Text('Sign Up',
+                                      style: TextStyle(
+                                          color: Colors.white, fontSize: 22)),
+                                  onPressed: () {
+                                    Navigator.push(context,
+                                        MaterialPageRoute(builder: (context) {
+                                      return RegisterPage();
+                                    }));
+                                  })),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             )
